8b95e685d3773d23035efa9449de8c4fb3769e5d
benni maxon <benni.maxon@gmail.com>
2022-11-30 14:28:28 -0800

    built event listeners in create.js

--- create/create.js
+++ create/create.js
@@ -5,10 +5,18 @@ const logoutButton = document.getElementById('logout');
 
 form.addEventListener('submit', async (e) => {
     // prevent default
+    e.preventDefault();
 
     // get the name and family id from the form
+    const data = new FormData(form);
 
+    const bunnyName = data.get('bunny-name');
+    const familyID = data.get('family-id');
     // use createBunny to create a bunny with this name and family id
+    await createBunny({
+        name: bunnyName,
+        family_id: familyID,
+    });
 
     form.reset();
 });
@@ -16,11 +24,19 @@ form.addEventListener('submit', async (e) => {
 window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
+    const selectEl = document.querySelector('select');
     // go get the families from supabase
+    const families = await getFamilies();
     // for each family
-    // create an option tag
-    // set the option's value and text content
-    // and append the option to the select
+    for (let family of families) {
+        // create an option tag
+        const familyOption = document.createElement('option');
+        // set the option's value and text content
+        familyOption.textContent = family.name;
+        familyOption.value = family.id;
+        // and append the option to the select
+        selectEl.append(familyOption);
+    }
 });
 
 checkAuth();
